6e3be907de34753ed4634ae28d029452ea1ee5e1
Bharat Ayyar <bayyar@gmail.com>
2013-12-19 13:30:46 -0500

    Edit to Category model fixing a typo

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveRecord::Base
 	has_many :article_categories
-	has_many :categories, through: :article_categories
+	has_many :articles, through: :article_categories
 	validates :category_name, presence: true
 	validates :category_name, uniqueness: true
 end
