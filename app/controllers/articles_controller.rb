8fc2de1454ccb0fbc0ea6f1824ff1041c7418e1d
Bharat Ayyar <bayyar@gmail.com>
2014-01-23 13:48:28 -0500

    Adds more Bootstrap styling to Article, Category and Welcome views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   def index
-  	@articles = Article.order(updated_at: :desc).limit(25)
+  	@articles = Article.order(updated_at: :desc).limit(10)
   end
 
   def show
