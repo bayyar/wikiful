d08b812855684e53f73b6ea8df6f60a4f1f086d5
Bharat Ayyar <bayyar@gmail.com>
2014-01-26 21:33:10 -0500

    Adds user authorization features for certain actions and changes routes to user-friendly names

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,14 @@ class ApplicationController < ActionController::Base
     @categories = Category.all.order(:category_name)
   end
 
+  private
+  def current_user
+  	@current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
+  def authorize
+  	redirect_to login_path, alert: "You need to be logged in to do that" if current_user.nil?
+  end
+
 end
